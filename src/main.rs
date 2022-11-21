5ac2902224d7112a06eb6cddca3d21039719140c
typable <contact@typable.dev>
2022-11-21 11:29:31 +0100

    Added list and get path feature

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::env::Args;
 use std::process::Command;
 
 use rc::prelude::*;
@@ -13,30 +14,72 @@ fn run() -> Result<()> {
     let config = Config::load()?;
     let mut args = env::args();
     args.next().unwrap();
-    if let Some(name) = args.next() {
-        if let Some((_, path)) = find_program(&config, &name) {
-            if let Err(err) = Command::new(config.editor).args([&path]).status() {
-                error!("{}", err);
-            }
-        } else {
-            warn!("No config found for {}!", name);
+    if let Some(arg) = args.next() {
+        match arg.as_str() {
+            "-l" => list_programs(&config),
+            "-p" => get_program_path(&config, &mut args),
+            _ => edit_program(&config, &arg),
         }
     } else {
-        let mut config_count = 0;
-        if let Some(programs) = config.programs {
-            config_count = programs.len();
-        }
-        info!("");
-        info!("\"rc\" - A config file management utility");
-        info!("");
-        info!("Usage: rc <program>");
-        info!("Use \"rc rc\" to add a new config file.");
-        info!("");
-        info!("Editor: {}", config.editor);
-        info!("Config files: {}", config_count);
-        info!("");
+        show_info();
     }
     Ok(())
+}
+
+fn list_programs(config: &Config) {
+    let mut total = 0;
+    let mut width = 2;
+    let mut lines = Vec::new();
+    if let Some(programs) = &config.programs {
+        total = programs.len();
+        for program in programs {
+            let (id, path) = program;
+            if width < id.len() {
+                width = id.len();
+            }
+            lines.push((id, path));
+        }
+    }
+    info!("");
+    info!("total config files: {}", total);
+    info!("");
+    warn!("{: <width$}  {}", "ID", "PATH", width = width);
+    for line in lines {
+        let (id, path) = line;
+        info!("{: <width$}  {}", id, path, width = width);
+    }
+    info!("");
+}
+
+fn get_program_path(config: &Config, args: &mut Args) {
+    if let Some(id) = args.next() {
+        if let Some((_, path)) = find_program(config, &id) {
+            info!("{}", path);
+        } else {
+            warn!("No config found for {}!", id);
+        }
+    } else {
+        warn!("Argument required! Usage: rc -p <program>");
+    }
+}
+
+fn edit_program(config: &Config, id: &str) {
+    if let Some((_, path)) = find_program(config, id) {
+        if let Err(err) = Command::new(&config.editor).args([&path]).status() {
+            error!("{}", err);
+        }
+    } else {
+        warn!("No config found for {}!", id);
+    }
+}
+
+fn show_info() {
+    info!("");
+    warn!("\"rc\" - A config file management utility");
+    info!("");
+    info!("Usage: rc <program>");
+    info!("Use \"rc rc\" to add a new config file.");
+    info!("");
 }
 
 fn find_program(config: &Config, name: &str) -> Option<(String, String)> {
